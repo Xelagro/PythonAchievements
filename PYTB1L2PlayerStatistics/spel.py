c524c6f1f0aba5408409ca362208234c955e452c
Sam <sammywels13@gmai.com>
2020-09-16 13:00:38 +0200

    Python

---
+++ PYTB1L2PlayerStatistics/spel.py
@@ -0,0 +1,20 @@
+a = "Sam Wels"
+print("Name: " + a)
+b = "300"
+print("Strength: " + b)
+c = "99"
+print("Lives left: " + c)
+d = "300"
+print("Speed " + d)
+e = "395"
+print("Points: " + e)
+f = "Smash"
+print("Power: " + f)
+g = "10000"
+print("Sneak: " + g)
+h = "6 left"
+print("Team: " + h)
+i = "16"
+print("Age: " + i)
+j = "false"
+print("Wanted: " + j)
