4b94178bf4765cf14bb95e679fc77cc6ff11329e
Sam <sammywels13@gmai.com>
2020-09-25 11:49:13 +0200

    Operationadd

---
+++ PYTB1L2MyFirstOperations/Sommen.py
@@ -0,0 +1,49 @@
+
+print("Plus")
+x = 6
+y = 3
+
+print(x + y)
+
+
+print("Min")
+x = 6
+y = 3
+
+print(x - y)
+
+
+print("Keer")
+x = 2
+y = 4
+
+print(x * y)
+
+
+print("Delen door")
+x = 10
+y = 5
+
+print(x / y)
+
+
+print("Hoeveel rest over.")
+x = 13
+y = 4
+
+print(x % y)
+
+
+print("Linker getal x de hoeveelheid van rechter getal.")
+x = 3
+y = 6
+
+print(x ** y)
+
+
+print("Rond af op een geheel nummer.")
+x = 11
+y = 5
+
+print(x // y)
+
